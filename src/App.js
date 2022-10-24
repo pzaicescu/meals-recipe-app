41a1660692310076d4f3c4989c220aa814ba26f9
Zaicescu Paul <paul.zaicescu@tickbird.org>
2022-10-24 13:36:07 +0300

    initial structure of the project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+import Favorites from "./components/Favorites";
+import Meals from "./components/Meals";
+import Modals from "./components/Modals";
+import Search from "./components/Search";
+
+function App() {
+    return (
+        <div>
+            {/*<Search/>*/}
+            {/*<Favorites/>*/}
+            <Meals/>
+            {/*<Modals/>*/}
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Modals.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Modals = () => {
+    return (
+        <div>
+            <h1>Modals component</h1>
+        </div>
+    );
+};
+
+export default Modals;
