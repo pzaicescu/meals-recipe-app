f60edd6349a20c618a0c14ed384bd1f1777b9f84
Zaicescu Paul <paul.zaicescu@tickbird.org>
2022-10-31 20:40:56 +0200

    data fetching and meals component

--- src/context.jsx
+++ src/context.jsx
@@ -1,9 +1,29 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
+import axios from 'axios'
 
 const AppContext = React.createContext();
 
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a';
+const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
 const AppProvider = ({children}) => {
-    return <AppContext.Provider value='hello'>
+
+    const [meals, setMeals] = useState([])
+
+    const fetchMeals = async (url) => {
+        try {
+            const {data} = await axios.get(url)
+            setMeals(data.meals)
+        } catch (e) {
+            console.log(e.response)
+        }
+    }
+
+    useEffect(() => {
+        fetchMeals(allMealsUrl)
+    }, [])
+
+    return <AppContext.Provider value={{meals}}>
         {children}
     </AppContext.Provider>
 };
