621dab2cfd1dda517c1aefacf2f3be598835b483
Zaicescu Paul <paul.zaicescu@tickbird.org>
2022-11-01 18:54:41 +0200

    adding all components

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import {useGlobalContext} from "../context";
+import {BsHandThumbsUp} from "react-icons/bs";
 
 const Meals = () => {
-    const {meals} = useGlobalContext()
+    const {loading, meals, selectMeal} = useGlobalContext()
+
+    if (loading) {
+        return <section className="section">
+            <h4>Loading... </h4>
+        </section>
+    }
+
+    if (meals.length < 1) {
+        return <section className="section">
+            <h4>No meals matched your searched term. Please try again.</h4>
+        </section>
+    }
+
     return (
-        <section>
+        <section className="section-center">
             {meals.map((singleMeal) => {
-                console.log(singleMeal)
-                return <h4>single meal</h4>
+                const {idMeal, strMeal: title, strMealThumb: image} = singleMeal
+                return <article key={idMeal} className="single-meal">
+                    <img src={image} alt="meal" className="img" onClick={() => selectMeal(idMeal)}/>
+                    <footer>
+                        <h5>{title}</h5>
+                        <button className="like-btn"><BsHandThumbsUp/></button>
+                    </footer>
+                </article>
+
             })}
         </section>
     );
 };
 
 export default Meals;
-//2:57:00
